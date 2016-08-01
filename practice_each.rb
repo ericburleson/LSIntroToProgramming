0737f9eafe7b9654f0441b67afbedd0a58cfc777
Eric Burleson <eric.burleson@gmail.com>
2016-08-01 18:07:40 -0500

    These exercises are from Loops

---
+++ practice_each.rb
@@ -0,0 +1,10 @@
+#practice_each.rb
+
+names = ['Bob', 'Dan', 'Mark', 'Fred', 'Tom']
+
+names.each do  |name| 
+  puts "#{x}. #{name}"
+  x =+ 1
+end
+
+
---
+++ useful_loop.rb
@@ -0,0 +1,8 @@
+#useful_loop.rb
+
+i = 0
+loop do |
+  |i += 1
+  puts i
+  break #this will cause the execution to exit the loop
+end
