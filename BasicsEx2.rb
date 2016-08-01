626f354f03c355b7487a7c76143c2d02a119f7d4
Eric Burleson <eric.burleson@gmail.com>
2016-08-01 11:42:28 -0500

    This program gives the digits from a four digit number

---
+++ BasicsEx2.rb
@@ -0,0 +1,11 @@
+#BasicsEx2.rb
+
+puts "Let's get the digits from 5467."
+puts "Here's the thousands digit"
+puts 5467 / 1000
+puts "Here's the hundreds digit"
+puts 5467 / 100 % 10
+puts "Here's the tens digit"
+puts 5467 / 10 % 10
+puts "Here's the ones digit"
+puts 5467 % 10
